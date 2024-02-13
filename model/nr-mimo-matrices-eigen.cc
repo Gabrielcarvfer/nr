8bfc98c80b80fffd61f10a2e7784d42c72a3ce1f
Gabriel Ferreira <gabrielcarvfer@gmail.com>
2024-02-13 11:39:09 +0100

    nr-mimo-matrices-eigen: Avoid allocating intermediary matrices

--- model/nr-mimo-matrices-eigen.cc
+++ model/nr-mimo-matrices-eigen.cc
@@ -38,24 +38,64 @@ NrCovMat::CalcIntfNormChannelMimo(const ComplexMatrixArray& chanMat) const
     return res;
 }
 
+auto res = ComplexMatrixArray {1,1,1};
+auto chanPrec = ComplexMatrixArray {1,1,1};
+auto chanPrecTrans = ComplexMatrixArray {1,1,1};
+auto chanCov = ComplexMatrixArray {1,1,1};
+
 ComplexMatrixArray
 NrIntfNormChanMat::ComputeMseMimo(const ComplexMatrixArray& precMats) const
 {
     auto nDims = precMats.GetNumCols();
     auto identity = Eigen::MatrixXcd::Identity(nDims, nDims);
-    auto res = ComplexMatrixArray{nDims, nDims, precMats.GetNumPages()};
-    auto chanPrec = (*this) * precMats;
-    auto chanCov = chanPrec.HermitianTranspose() * chanPrec;
+    res.SetDimensions(nDims, nDims, precMats.GetNumPages());
+    // Temporary tables only need a single page, since we do the calculations per page anyways
+    chanPrec.SetDimensions(this->GetNumRows(), precMats.GetNumCols(), 1);
+    chanPrecTrans.SetDimensions(chanPrec.GetNumCols(), chanPrec.GetNumRows(), 1);
+    chanCov.SetDimensions(chanPrecTrans.GetNumRows(), chanPrec.GetNumCols(), 1);
+
+    EigenMatrix<std::complex<double>> chanPrecEigen(chanPrec.GetPagePtr(0),
+                                                    chanPrec.GetNumRows(),
+                                                    chanPrec.GetNumCols());
+    EigenMatrix<std::complex<double>> chanPrecTransEigen(chanPrecTrans.GetPagePtr(0),
+                                                         chanPrecTrans.GetNumRows(),
+                                                         chanPrecTrans.GetNumCols());
+    EigenMatrix<std::complex<double>> chanCovEigen(chanCov.GetPagePtr(0),
+                                                   chanCov.GetNumRows(),
+                                                   chanCov.GetNumCols());
+    Eigen::MatrixXcd temp(chanCov.GetNumRows(), chanCov.GetNumCols());
     for (size_t iRb = 0; iRb < res.GetNumPages(); iRb++)
     {
-        ConstEigenMatrix<std::complex<double>> chanCovEigen(chanCov.GetPagePtr(iRb),
-                                                            chanCov.GetNumRows(),
-                                                            chanCov.GetNumCols());
+        ConstEigenMatrix<std::complex<double>> thisEigen(this->GetPagePtr(iRb),
+                                                         this->GetNumRows(),
+                                                         this->GetNumCols());
+        ConstEigenMatrix<std::complex<double>> precMatsEigen(precMats.GetPagePtr(iRb),
+                                                             precMats.GetNumRows(),
+                                                             precMats.GetNumCols());
+        // Compute the following product without intermediary allocations
+        //chanPrec = (*this) * precMats;
+        chanPrecEigen.noalias() = thisEigen * precMatsEigen;
+
+        // Compute the transposed matrix without intermediary allocations
+        //chanPrecTrans = chanPrec.HermitianTranspose();
+
+        chanPrecTransEigen.noalias() = chanPrecEigen.transpose();
+        // Compute the conjugates of the transposed matrix to make it Hermitian
+        // chanPrecTransposed = chanPrec.HermitianTranspose();
+        auto& chanPrecTransValues = chanPrecTrans.GetValuesRef();
+        for (size_t value = 0; value < chanPrecTrans.GetSize(); value++)
+        {
+            chanPrecTransValues[value] = std::conj(chanPrecTransValues[value]);
+        }
+        // Compute the product
+        //chanCov = chanPrecTrans * chanPrec;
+        chanCovEigen.noalias() = chanPrecTransEigen * chanPrecEigen;
+
+        // Calculate whatever
         EigenMatrix<std::complex<double>> resEigen(res.GetPagePtr(iRb),
                                                    res.GetNumRows(),
                                                    res.GetNumCols());
-        Eigen::MatrixXcd temp = chanCovEigen + identity;
-        resEigen = temp.selfadjointView<Eigen::Lower>().llt().solve(identity);
+        temp.noalias() = chanCovEigen + identity;
     }
     return res;
 }
