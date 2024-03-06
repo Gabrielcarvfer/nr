19aa3679bd0d55950fe2f7474e5a1535743da728
Gabriel Ferreira <gabrielcarvfer@gmail.com>
2024-03-06 12:49:12 +0100

    nr-mimo-matrices-*: Implement rank and optimal precoding matrix extraction

--- model/nr-mimo-matrices-no-eigen.cc
+++ model/nr-mimo-matrices-no-eigen.cc
@@ -23,4 +23,22 @@ NrIntfNormChanMat::ComputeMseMimo([[maybe_unused]] const ComplexMatrixArray& pre
     NS_FATAL_ERROR("MIMO MSE computation requires Eigen matrix library.");
 }
 
+uint8_t
+NrIntfNormChanMat::GetEigenWidebandRank([[maybe_unused]] double thr) const
+{
+    NS_FATAL_ERROR("GetEigenWidebandRank requires Eigen matrix library.");
+}
+
+std::vector<uint8_t>
+NrIntfNormChanMat::GetEigenSubbandRanks([[maybe_unused]] double thr) const
+{
+    NS_FATAL_ERROR("GetEigenSubbandRanks requires Eigen matrix library.");
+}
+
+ComplexMatrixArray
+NrIntfNormChanMat::ExtractOptimalPrecodingMatrices(uint8_t rank) const
+{
+    NS_FATAL_ERROR("GetEigenRank requires Eigen matrix library.");
+}
+
 } // namespace ns3
